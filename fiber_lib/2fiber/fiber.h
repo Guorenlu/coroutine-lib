8a7b9af53dbac9abbb5ec4449b74998b9748a421
Guorenlu <3224265849@qq.com>
2025-08-06 19:05:47 +0800

    初始化

--- fiber_lib/2fiber/fiber.h
+++ fiber_lib/2fiber/fiber.h
@@ -1,84 +1,90 @@
 #ifndef _COROUTINE_H_
 #define _COROUTINE_H_
 
-#include <iostream>     
-#include <memory>       
-#include <atomic>       
-#include <functional>   
-#include <cassert>      
-#include <ucontext.h>   
+#include <iostream>
+#include <memory>
+#include <atomic>
+#include <functional>
+#include <cassert>
+#include <ucontext.h>
 #include <unistd.h>
 #include <mutex>
 
-namespace sylar {
-
-class Fiber : public std::enable_shared_from_this<Fiber>
+namespace sylar
 {
-public:
-	// 协程状态
-	enum State
+
+	// Fiber 类：协程的实现
+	// 提供协程的创建、切换和销毁功能
+	class Fiber : public std::enable_shared_from_this<Fiber>
 	{
-		READY, 
-		RUNNING, 
-		TERM 
+	public:
+		// 协程状态枚举
+		enum State
+		{
+			READY,	 // 准备运行
+			RUNNING, // 正在运行
+			TERM		 // 已结束
+		};
+
+	private:
+		// 私有构造函数：仅用于创建主协程
+		Fiber();
+
+	public:
+		// 构造函数：创建子协程
+		Fiber(std::function<void()> cb, size_t stacksize = 0, bool run_in_scheduler = true);
+		~Fiber();
+
+		// 重置协程：复用已结束的协程
+		void reset(std::function<void()> cb);
+
+		// 恢复协程：切换到当前协程运行
+		void resume();
+		// 让出协程：切换到调度协程或主协程
+		void yield();
+
+		// 获取协程 ID
+		uint64_t getId() const { return m_id; }
+		// 获取协程状态
+		State getState() const { return m_state; }
+
+	public:
+		// 设置当前运行的协程
+		static void SetThis(Fiber *f);
+
+		// 获取当前运行的协程
+		static std::shared_ptr<Fiber> GetThis();
+
+		// 设置调度协程（默认为主协程）
+		static void SetSchedulerFiber(Fiber *f);
+
+		// 获取当前运行的协程 ID
+		static uint64_t GetFiberId();
+
+		// 协程函数：协程的入口函数
+		static void MainFunc();
+
+	private:
+		// 协程 ID
+		uint64_t m_id = 0;
+		// 栈大小
+		uint32_t m_stacksize = 0;
+		// 协程状态
+		State m_state = READY;
+		// 协程上下文
+		ucontext_t m_ctx;
+		// 协程栈指针
+		void *m_stack = nullptr;
+		// 协程函数
+		std::function<void()> m_cb;
+		// 是否运行在调度协程中
+		bool m_runInScheduler;
+
+	public:
+		// 协程的互斥锁
+		std::mutex m_mutex;
 	};
-
-private:
-	// 仅由GetThis()调用 -> 私有 -> 创建主协程  
-	Fiber();
-
-public:
-	Fiber(std::function<void()> cb, size_t stacksize = 0, bool run_in_scheduler = true);
-	~Fiber();
-
-	// 重用一个协程
-	void reset(std::function<void()> cb);
-
-	// 任务线程恢复执行
-	void resume();
-	// 任务线程让出执行权
-	void yield();
-
-	uint64_t getId() const {return m_id;}
-	State getState() const {return m_state;}
-
-public:
-	// 设置当前运行的协程
-	static void SetThis(Fiber *f);
-
-	// 得到当前运行的协程 
-	static std::shared_ptr<Fiber> GetThis();
-
-	// 设置调度协程（默认为主协程）
-	static void SetSchedulerFiber(Fiber* f);
-	
-	// 得到当前运行的协程id
-	static uint64_t GetFiberId();
-
-	// 协程函数
-	static void MainFunc();	
-
-private:
-	// id
-	uint64_t m_id = 0;
-	// 栈大小
-	uint32_t m_stacksize = 0;
-	// 协程状态
-	State m_state = READY;
-	// 协程上下文
-	ucontext_t m_ctx;
-	// 协程栈指针
-	void* m_stack = nullptr;
-	// 协程函数
-	std::function<void()> m_cb;
-	// 是否让出执行权交给调度协程
-	bool m_runInScheduler;
-
-public:
-	std::mutex m_mutex;
-};
 
 }
 
 #endif
-
--- fiber_lib/6hook/fd_manager.h
+++ fiber_lib/6hook/fd_manager.h
@@ -3,94 +3,94 @@
 
 #include <memory>
 #include <shared_mutex>
+#include <string>
 #include "thread.h"
 
-
-namespace sylar{
-
-// fd info
-class FdCtx : public std::enable_shared_from_this<FdCtx>
+namespace sylar
 {
-private:
-	bool m_isInit = false;
-	bool m_isSocket = false;
-	bool m_sysNonblock = false;
-	bool m_userNonblock = false;
-	bool m_isClosed = false;
-	int m_fd;
 
-	// read event timeout
-	uint64_t m_recvTimeout = (uint64_t)-1;
-	// write event timeout
-	uint64_t m_sendTimeout = (uint64_t)-1;
+	// fd info
+	class FdCtx : public std::enable_shared_from_this<FdCtx>
+	{
+	private:
+		bool m_isInit = false;
+		bool m_isSocket = false;
+		bool m_sysNonblock = false;
+		bool m_userNonblock = false;
+		bool m_isClosed = false;
+		int m_fd;
 
-public:
-	FdCtx(int fd);
-	~FdCtx();
+		// read event timeout
+		uint64_t m_recvTimeout = (uint64_t)-1;
+		// write event timeout
+		uint64_t m_sendTimeout = (uint64_t)-1;
 
-	bool init();
-	bool isInit() const {return m_isInit;}
-	bool isSocket() const {return m_isSocket;}
-	bool isClosed() const {return m_isClosed;}
+	public:
+		FdCtx(int fd);
+		~FdCtx();
 
-	void setUserNonblock(bool v) {m_userNonblock = v;}
-	bool getUserNonblock() const {return m_userNonblock;}
+		bool init();
+		bool isInit() const { return m_isInit; }
+		bool isSocket() const { return m_isSocket; }
+		bool isClosed() const { return m_isClosed; }
 
-	void setSysNonblock(bool v) {m_sysNonblock = v;}
-	bool getSysNonblock() const {return m_sysNonblock;}
+		void setUserNonblock(bool v) { m_userNonblock = v; }
+		bool getUserNonblock() const { return m_userNonblock; }
 
-	void setTimeout(int type, uint64_t v);
-	uint64_t getTimeout(int type);
-};
+		void setSysNonblock(bool v) { m_sysNonblock = v; }
+		bool getSysNonblock() const { return m_sysNonblock; }
 
-class FdManager
-{
-public:
-	FdManager();
+		void setTimeout(int type, uint64_t v);
+		uint64_t getTimeout(int type);
+	};
 
-	std::shared_ptr<FdCtx> get(int fd, bool auto_create = false);
-	void del(int fd);
+	class FdManager
+	{
+	public:
+		FdManager();
 
-private:
-	std::shared_mutex m_mutex;
-	std::vector<std::shared_ptr<FdCtx>> m_datas;
-};
+		std::shared_ptr<FdCtx> get(int fd, bool auto_create = false);
+		void del(int fd);
 
+	private:
+		std::shared_mutex m_mutex;
+		std::vector<std::shared_ptr<FdCtx>> m_datas;
+	};
 
-template<typename T>
-class Singleton
-{
-private:
-    static T* instance;
-    static std::mutex mutex;
+	template <typename T>
+	class Singleton
+	{
+	private:
+		static T *instance;
+		static std::mutex mutex;
 
-protected:
-    Singleton() {}  
+	protected:
+		Singleton() {}
 
-public:
-    // Delete copy constructor and assignment operation
-    Singleton(const Singleton&) = delete;
-    Singleton& operator=(const Singleton&) = delete;
+	public:
+		// Delete copy constructor and assignment operation
+		Singleton(const Singleton &) = delete;
+		Singleton &operator=(const Singleton &) = delete;
 
-    static T* GetInstance() 
-    {
-        std::lock_guard<std::mutex> lock(mutex); // Ensure thread safety
-        if (instance == nullptr) 
-        {
-            instance = new T();
-        }
-        return instance;
-    }
+		static T *GetInstance()
+		{
+			std::lock_guard<std::mutex> lock(mutex); // Ensure thread safety
+			if (instance == nullptr)
+			{
+				instance = new T();
+			}
+			return instance;
+		}
 
-    static void DestroyInstance() 
-    {
-        std::lock_guard<std::mutex> lock(mutex);
-        delete instance;
-        instance = nullptr;
-    }
-};
+		static void DestroyInstance()
+		{
+			std::lock_guard<std::mutex> lock(mutex);
+			delete instance;
+			instance = nullptr;
+		}
+	};
 
-typedef Singleton<FdManager> FdMgr;
+	typedef Singleton<FdManager> FdMgr;
 
 }
 
